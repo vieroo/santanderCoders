4cf2bf4547c9959b228bb2123e04361c8df945f2
Eduardo Viero <duduviero@gmail.com>
2024-06-10 11:41:20 -0300

    Componentização

---
+++ ReactJs/aula1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+import "./styles/App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <Navbar />
+      </header>
+    );
+  }
+}
+
+export default App;
---
+++ ReactJs/aula1/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logoImg from "../../assets/logo.png";
+import "./styles.css";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav id="navbar">
+        <div className="nav-brand">
+          <img src={logoImg} alt="" />
+          <h1>Space Flight News</h1>
+        </div>
+
+        <ul className="nav-list">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Trending</a>
+          </li>
+          <li>
+            <a href="/">Categories</a>
+          </li>
+          <li>
+            <a href="/">About Us</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
