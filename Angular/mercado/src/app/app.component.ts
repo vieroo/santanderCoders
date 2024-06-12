63e1501a8e7398ccfa6a6c4956d708851f4f37dc
Eduardo Viero <duduviero@gmail.com>
2024-06-12 13:24:52 -0300

    Angular - Rotas

--- Angular/mercado/src/app/app.component.ts
+++ Angular/mercado/src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeModule } from './home/home.module';
 import { HomeComponent } from './home/home.component';
+import { SobreModule } from './sobre/sobre.module';
+import { SobreComponent } from './sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeModule, HomeComponent],
+  imports: [
+    RouterOutlet,
+    HomeModule,
+    HomeComponent,
+    SobreModule,
+    SobreComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- Angular/mercado/src/app/app.routes.ts
+++ Angular/mercado/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
